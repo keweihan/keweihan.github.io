e8fb4f5fb382af6dba270277202a920153c98d24
Kewei Han <hankevin@umich.edu>
2023-11-23 22:45:16 -0500

    use dropdowns

--- static/javascript/indexs.js
+++ static/javascript/indexs.js
@@ -1,3 +1,4 @@
+
 // Insert iframe if it doesn't already exist
 function insertBrowser()
 {
@@ -10,7 +11,7 @@ function insertBrowser()
 }
 
 
-// OnClick() function
+// OnClick() animation function
 function openURL(url)
 {
     // Insert iframe browser and make wider 
@@ -44,3 +45,34 @@ function openURL(url)
     );
     document.getElementById("browser").src = url;   
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all dropdown buttons
+    var dropdowns = document.querySelectorAll('.dropbtn');
+  
+    dropdowns.forEach(function(dropbtn) {
+      dropbtn.addEventListener('click', function() {
+        // Get the target from the data attribute
+        var targetId = this.getAttribute('data-target');
+        var content = document.getElementById(targetId);
+        var $content = $(content);
+  
+        // Check if the dropdown is currently visible
+        if ($content.is(':visible')) {
+          // Animate hiding
+          Velocity(content, "slideUp");
+        } else {
+          // Animate showing
+          Velocity(content, "slideDown");
+          // Hide other dropdowns
+          dropdowns.forEach(function(otherDropBtn) {
+            var otherTargetId = otherDropBtn.getAttribute('data-target');
+            if (otherTargetId !== targetId) {
+              var otherContent = document.getElementById(otherTargetId);
+              Velocity(otherContent, "slideUp");
+            }
+          });
+        }
+      });
+    });
+  });
