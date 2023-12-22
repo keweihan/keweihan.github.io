2060d0af7a7925bd58c144e120b4b90103aa20d6
Kewei Han <hankevin@umich.edu>
2023-12-21 22:13:57 -0500

    css structure cleanup

--- static/javascript/warpball.js
+++ static/javascript/warpball.js
@@ -1,16 +1,13 @@
-// Use different Warpball stylesheet depending on if inside iframe
+// Use different stylesheet depending on if inside iframe
 function injectCSS() {
-    // css elements common to both inside/outside iframe
-    document.head.insertAdjacentHTML("beforeend","<link rel='stylesheet' href='static/css/warpball-general.css'>")
-
     if (window.top!=window.self)
     {
         // In a Frame or IFrame
-        document.head.insertAdjacentHTML("beforeend","<link rel='stylesheet' href='static/css/warpball-iframe.css'>")
+        document.head.insertAdjacentHTML("beforeend","<link rel='stylesheet' href='static/css/style-iframe.css'>")
     }
     else
     {
         // Not in a frame
-        document.head.insertAdjacentHTML("beforeend","<link rel='stylesheet' href='static/css/warpball-noiframe.css'>")
+        document.head.insertAdjacentHTML("beforeend","<link rel='stylesheet' href='static/css/style-noiframe.css'>")
     } 
 }
